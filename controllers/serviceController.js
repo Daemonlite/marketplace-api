24a1f5654dc923f09b8caad9609daf9b12f227c7
Daemonlite <khemikal2016@gmail.com>
2025-08-16 11:40:36 +0000

    services crud update

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,161 +1,187 @@
 import ServiceCategory from "../models/ServiceCategory.js";
 import Service from "../models/Service.js";
 import User from "../models/User.js";
-import { uploadImageLater } from "../queues/index.js";
+import { uploadImage } from "../helpers/uploadImage.js";
 
+export const getServices = async (req, res) => {
+  try {
+    const services = await Service.find().populate({
+      path: "category provider",
+      select: "_id name",
+      
 
-export const getServices = async (req,res) => {
-    try {
+    }).sort({ createdAt: -1 });
+    return res.status(200).json(services);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-        const services = await Service.find().populate({
-            path: 'category',
-            select: '_id name',
-        });
-        return res.status(200).json(services);
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+export const getServiceById = async (req, res) => {
+  try {
+    const service = await Service.findById(req.params.id).populate({
+      path: "category",
+      select: "_id name",
+    })
+
+    if (!service) {
+      return res.status(404).json({ message: "Service not found" });
     }
-}
 
+    return res.status(200).json(service);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const getServiceById = async (req,res) => {
-    try {
-        const service = await Service.findById(req.params.id).populate({
-            path: 'category',
-            select: '_id name',
-        });
-        return res.status(200).json(service);
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+export const fetchServiceProviders = async (req, res) => {
+  try {
+    const serviceId = req.params.id;
+
+    // Get distinct provider IDs from services in this category
+    const providerIds = await Service.find({ category: serviceId }).distinct("provider");
+
+    if (!providerIds || providerIds.length === 0) {
+      return res.status(404).json({ message: "No providers found for this category" });
     }
-}
 
-export const fetchServiceProviders = async(req,res) => {
-    try {
+    // Fetch all related users
+    const relatedUsers = await User.find({ _id: { $in: providerIds } })
+      .select("_id name email phone"); // pick fields you want
 
-        const serviceId = req.params.id;
-        const providers = await Service.findById(serviceId).populate({
-            path: 'provider',
-            select: '_id name email phone',
-        })
+    return res.status(200).json(relatedUsers);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-        return res.status(200).json(providers);
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
+
+export const fetchUserServices = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const relatedUsers = await User.find({ _id: userId });
+    if (!relatedUsers) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
 
-export const fetchUserServices = async(req,res) => {
+    const services = await Service.find({ provider: userId }).sort({ createdAt: -1 });
 
-    try {
+    return res.status(200).json(services);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-        const userId = req.params.id;
+export const createService = async (req, res) => {
+  try {
+    const { name, description, category, location, keyServices } =
+      req.body;
 
-        const relatedUsers = await User.find({ _id: userId });
-        if(!relatedUsers){
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const services = await Service.find({ provider: userId });
-
-        return res.status(200).json(services);
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
+    const image = req.file;
+    if (!name || !description || !category || !location || !keyServices) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    
-}
 
-
-export const createService = async (req,res) => {
-    try {
-
-        const { name, description, category,location, keyServices,image } = req.body
-        if (!name || !description || !category || !location || !keyServices) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const currentUser = req.user._id;
-        const user = await User.findById(currentUser);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        if(user.isBlocked){
-            return res.status(403).json({ message: "You are blocked by the admin" });
-        }
-        const serviceCategory = await ServiceCategory.findById(category);
-        if (!serviceCategory) {
-            return res.status(404).json({ message: "Service category not found" });
-        }
-        const service = await Service.create({ name, description, category,location, keyServices,image });
-        if (image) {
-            await uploadImageLater(image);
-        }
-        return res.status(200).json(service);
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+    const currentUser = req.user._id;
+    const user = await User.findById(currentUser);
+    if (!user || !user.role === "provider") {
+      return res.status(404).json({ message: "User not found" });
     }
-}
 
-export const updateService = async (req,res) => {
-    try {
-
-        const serviceId = req.params.id;
-        const body = req.body;
-
-        const currentUser = req.user._id;
-        const user = await User.findById(currentUser);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        if(user.isBlocked){
-            return res.status(403).json({ message: "You are blocked by the admin" });
-        }
-
-        const service = Service.findById(serviceId);
-        if (!service) {
-            return res.status(404).json({ message: "Service not found" });
-        }
-        const updatedService = await Service.findByIdAndUpdate(serviceId, body, { new: true });
-        return res.status(200).json(updatedService);
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+    if (user.isBlocked) {
+      return res.status(403).json({ message: "You are blocked by the admin" });
     }
-}
-
-export const deleteService = async (req,res) => {
-    try {
-
-        const serviceId = req.params.id;
-        const service = await Service.findById(serviceId);
-        if (!service) {
-            return res.status(404).json({ message: "Service not found" });
-        }
-
-        const currentUser = req.user._id;
-
-        if (service.provider.toString() !== currentUser.toString() && req.user.role !== "admin") {
-            return res.status(403).json({ message: "You are not authorized to delete this service" });
-        }
-
-        await service.remove();
-        return res.status(200).json({ message: "Service deleted successfully" });
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+    const serviceCategory = await ServiceCategory.findById(category);
+    if (!serviceCategory) {
+      return res.status(404).json({ message: "Service category not found" });
     }
-}
+
+    const existingService = await Service.findOne({ name, provider: user._id });
+    if (existingService) {
+      return res.status(400).json({ message: "Service already exists" });
+    }
+
+    const services = keyServices.split(",");
+    console.log(`services`, services);
+    let imageUrl;
+    if (image) {
+      imageUrl = await uploadImage(image);
+    }
+    const service = await Service.create({
+      name,
+      description,
+      category,
+      location,
+      keyServices: services,
+      image: imageUrl,
+      provider: user._id,
+    });
+    return res.status(200).json(service);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateService = async (req, res) => {
+  try {
+    const serviceId = req.params.id;
+    const body = req.body;
+
+    const currentUser = req.user._id;
+    const user = await User.findById(currentUser);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    let imageUrl;
+    if (req.file) {
+      imageUrl = await uploadImage(req.file);
+      body.image = imageUrl;
+    }
+
+    const service = Service.findById(serviceId);
+    if (!service) {
+      return res.status(404).json({ message: "Service not found" });
+    }
+    const updatedService = await Service.findByIdAndUpdate(serviceId, body, {
+      new: true,
+    });
+    return res.status(200).json(updatedService);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteService = async (req, res) => {
+  try {
+    const serviceId = req.params.id;
+    const service = await Service.findById(serviceId);
+    if (!service) {
+      return res.status(404).json({ message: "Service not found" });
+    }
+
+    const currentUser = req.user._id;
+
+    if (
+      service.provider.toString() !== currentUser.toString() &&
+      req.user.role !== "admin"
+    ) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this service" });
+    }
+
+    await service.deleteOne();
+    return res.status(200).json({ message: "Service deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import verifyToken from "../middlewares/verify.js";
+import upload from "../middlewares/multer.js";
 import {getServices, getServiceById,fetchServiceProviders, fetchUserServices,createService, updateService, deleteService} from "../controllers/serviceController.js"
 
 const serviceRoutes = express.Router();
@@ -9,8 +10,8 @@ serviceRoutes.get("/", verifyToken, getServices);
 serviceRoutes.get("/:id", verifyToken, getServiceById);
 serviceRoutes.get("/service-providers/:id", verifyToken, fetchServiceProviders);
 serviceRoutes.get("/user-services/:id", verifyToken, fetchUserServices);
-serviceRoutes.post("/", verifyToken, createService);
-serviceRoutes.put("/:id", verifyToken, updateService);
+serviceRoutes.post("/", verifyToken,upload.single("image"), createService);
+serviceRoutes.patch("/:id", verifyToken, upload.single("image"),updateService);
 serviceRoutes.delete("/:id", verifyToken, deleteService);
 
 export default serviceRoutes
